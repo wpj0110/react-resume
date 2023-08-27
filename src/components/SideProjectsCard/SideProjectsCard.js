bad20f07a55077a998f5fe57fb74629e06923865
William Javier <wpj0110@gmail.com>
2023-08-27 18:40:21 -0500

    Skill Card Refactor, Added New Project

--- src/components/SideProjectsCard/SideProjectsCard.js
+++ src/components/SideProjectsCard/SideProjectsCard.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import './SideProjectsCard.css';
+import { openLinkInNewTab } from '../Helpers';
 
 function SideProjectsCard(props) {
 
   return (
     <div className="side-projects-card">
         <div className="project-title" onClick={() =>openLinkInNewTab(props.item.getUrl())}>{props.item.getTitle()}</div>
-        <div className="project-description">{props.item.getDescription()}</div>
+        {props.item.getDescription()}
     </div>
   );
 
-  function openLinkInNewTab(url) {
-    window.open(`//${url}`, '_blank');
-  }
 }
 
 export default SideProjectsCard;
